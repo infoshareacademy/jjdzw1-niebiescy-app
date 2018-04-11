6aa413e5fa3ebcb1ea4f2d3f55284f6a5a064cb6
Marcin Jackowiak <marcin.jackowiak94@gmail.com>
2018-04-11 20:24:56 +0200

    Added MeteoDataWriter json output file in valid format

--- src/main/java/MeteoDataWriter.java
+++ src/main/java/MeteoDataWriter.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.google.gson.*;
+
 
 public class MeteoDataWriter {
 
@@ -6,23 +13,41 @@ public class MeteoDataWriter {
     public String jsonWriter() {
 
         String jsonStringHolder;
-
-        jsonStringHolder =  dataWriter.fromURL("");
-
+        jsonStringHolder = dataWriter.fromURL("");
 
 
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonParser jp = new JsonParser();
+        JsonElement je = jp.parse(jsonStringHolder);
+        String prettyJsonString = gson.toJson(je);
+
+        try {
+
+            String filename = "daneIMGW.json";
+            String workingDirectory = System.getProperty("user.dir");
 
 
+            String absoluteFilePath = "";
 
 
+            absoluteFilePath = workingDirectory + File.separator + "/src/main/resources/" + filename;
 
+            System.out.println("Final filepath : " + absoluteFilePath);
+
+
+            FileWriter writer = new FileWriter(absoluteFilePath);
+            writer.write(prettyJsonString);
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         return jsonStringHolder;
 
 
     }
-
 
 
 }
