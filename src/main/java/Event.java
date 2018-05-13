e417adff3914eb4376f37843f026005ff2511435
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-05-13 20:09:30 +0200

    Event class added.

---
+++ src/main/java/Event.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+class Event {
+
+    String name;
+    String description;
+    Date startDate;
+    Date finishDate;
+    String address;
+    Integer organizer;
+    Double price;
+    Integer tickets;
+}
