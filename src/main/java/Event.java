7fe87e2e8cb3580e0b57cd0febf98564ffec569a
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-05-15 17:53:24 +0200

    Event class updated.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,12 +2,76 @@ import java.util.Date;
 
 class Event {
 
-    String name;
-    String description;
-    Date startDate;
-    Date finishDate;
-    String address;
-    Integer organizer;
-    Double price;
-    Integer tickets;
+    private String name;
+    private String description;
+    private Date startDate;
+    private Date finishDate;
+    private String address;
+    private Integer organizer;
+    private Double price;
+    private Integer tickets;
+
+     String getName() {
+        return name;
+    }
+
+     void setName(String name) {
+        this.name = name;
+    }
+
+     String getDescription() {
+        return description;
+    }
+
+     void setDescription(String description) {
+        this.description = description;
+    }
+
+     Date getStartDate() {
+        return startDate;
+    }
+
+     void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+     Date getFinishDate() {
+        return finishDate;
+    }
+
+     void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+
+     String getAddress() {
+        return address;
+    }
+
+     void setAddress(String address) {
+        this.address = address;
+    }
+
+     Integer getOrganizer() {
+        return organizer;
+    }
+
+     void setOrganizer(Integer organizer) {
+        this.organizer = organizer;
+    }
+
+     Double getPrice() {
+        return price;
+    }
+
+     void setPrice(Double price) {
+        this.price = price;
+    }
+
+     Integer getTickets() {
+        return tickets;
+    }
+
+     void setTickets(Integer tickets) {
+        this.tickets = tickets;
+    }
 }
--- src/main/java/EventsList.java
+++ src/main/java/EventsList.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class EventsList extends Event {
+class EventsList extends Event {
 
     List<Event> eventsList = new ArrayList<>();
 }
